4f72c6db7bb5570177144fedba354ea6b8359033
d9nich <dan.galaiko@gmail.com>
2020-07-10 21:38:37 +0300

    Exercise1 has been done. A CSS style sheet that defines a class for white fill and black stroke color and an id for red stroke and green color. A program that displays four circles and uses the style class and id.

--- src/sample/exercise1/Circles.java
+++ src/sample/exercise1/Circles.java
@@ -1,9 +1,12 @@
 package sample.exercise1;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
@@ -17,6 +20,7 @@ public class Circles extends Application {
     public void start(Stage primaryStage) {
         HBox pane = new HBox();
         pane.setSpacing(10);
+        pane.setPadding(new Insets(10, 10, 10, 10));
         pane.setAlignment(Pos.CENTER);
         final Scene scene = new Scene(pane);
         scene.getStylesheets().add("sample/exercise1/circles.css");
@@ -25,14 +29,18 @@ public class Circles extends Application {
         Circle second = new Circle(50);
         Circle third = new Circle(50);
         Circle fourth = new Circle(50);
+        Pane p1 = new StackPane(first);
+        Pane p2 = new StackPane(fourth);
 
         first.getStyleClass().addAll("whiteCircle", "blackRectangle");
         second.getStyleClass().add("whiteCircle");
         third.setId("redCircle");
         fourth.setId("blackCircle");
         fourth.getStyleClass().add("blackRectangle");
+        p1.getStyleClass().add("blackRectangle");
+        p2.getStyleClass().add("blackRectangle");
 
-        pane.getChildren().addAll(first, second, third, fourth);
+        pane.getChildren().addAll(p1, second, third, p2);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Exercise 1");
         primaryStage.show();
