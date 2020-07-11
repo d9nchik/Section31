ab41c0034daff2503e9b8361010aeb0d46ae6aeb
d9nich <dan.galaiko@gmail.com>
2020-07-11 18:20:00 +0300

    Exercise8 has been done. A program that displays an ellipse and scaling operations on it.

---
+++ src/sample/exercise8/ScaleController.java
@@ -0,0 +1,20 @@
+package sample.exercise8;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Ellipse;
+
+
+public class ScaleController {
+    @FXML
+    private TextField x, y;
+
+    @FXML
+    private Ellipse cylinder;
+
+    @FXML
+    private void scaleCylinder(){
+        cylinder.setScaleX(Double.parseDouble(x.getText()));
+        cylinder.setScaleY(Double.parseDouble(y.getText()));
+    }
+}
