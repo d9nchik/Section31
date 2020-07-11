e076a8d86cef5db9e219386f95c30e39ec144594
d9nich <dan.galaiko@gmail.com>
2020-07-11 16:50:02 +0300

    Exercise3 has been done. Generator of three different with CSS.

--- src/sample/exercise3/ThreeCards.java
+++ src/sample/exercise3/ThreeCards.java
@@ -1,5 +1,36 @@
 package sample.exercise3;
 
-public class ThreeCards {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ThreeCards extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        HBox pane = new HBox();
+        pane.getStylesheets().add("sample/exercise3/threeCards.css");
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 1; i <= 54; i++)
+            arrayList.add(i);
+        Collections.shuffle(arrayList);
+
+        for (int i = 0; i < 3; i++)
+            pane.getChildren().add(new ImageView("image/card/" + arrayList.get(i) + ".png"));
+
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Exercise 3"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show();
+    }
 }
